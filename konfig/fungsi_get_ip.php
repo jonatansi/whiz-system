99995ba2a50d64ae4ed69aa7e3d891436c4957c1
Jonatan <jonatanusu@gmail.com>
2024-03-06 23:43:37 +0700

    Update konfig

---
+++ konfig/fungsi_get_ip.php
@@ -0,0 +1,52 @@
+<?php
+function getUserIP()
+{
+    // Get real visitor IP behind CloudFlare network
+    if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+              $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+              $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+    }
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
+
+function getBrowser()
+{
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    $browser = "N/A";
+
+    $browsers = [
+        '/msie/i' => 'Internet explorer',
+        '/firefox/i' => 'Firefox',
+        '/safari/i' => 'Safari',
+        '/chrome/i' => 'Chrome',
+        '/edge/i' => 'Edge',
+        '/opera/i' => 'Opera',
+        '/mobile/i' => 'Mobile browser',
+    ];
+
+    foreach ($browsers as $regex => $value) {
+        if (preg_match($regex, $user_agent)) {
+            $browser = $value;
+        }
+    }
+
+    return $browser;
+}
+?>
--- konfig/koneksi.php
+++ konfig/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("103.167.236.233","whizdigital","JO2205ys12,.","whiz_system");
+$conn = mysqli_connect("103.167.237.229","uv-tel","JO2205ys12,.","staging");
 // Check connection
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
