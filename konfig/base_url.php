3c11a770997d20efc8ece1228f88e6daa9d89a99
Jonatan <jonatanusu@gmail.com>
2024-03-06 23:44:53 +0700

    Ulang dari awal

--- konfig/base_url.php
+++
@@ -1,5 +0,0 @@
-<?php
-$BASE_URL = "http://localhost:8080/whiz/system";
-$BASE_URL_MASTER = "http://localhost:8080/whiz/system/master";
-$BASE_URL_STOCK = "http://localhost:8080/whiz/system/stock";
-?>
--- konfig/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("103.167.237.229","uv-tel","JO2205ys12,.","staging");
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
-?>
