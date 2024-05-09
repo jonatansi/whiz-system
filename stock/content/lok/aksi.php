4f40607e75bcc4ecefeffa6ab795c394a2f4dccd
root <root@staging.uv-tel.com>
2024-05-09 16:57:49 +0000

    Update lokasi

--- stock/content/lok/aksi.php
+++ stock/content/lok/aksi.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 // error_reporting(0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (empty($_SESSION['login_user'])){
 	header('location:keluar');
 }
